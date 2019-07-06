c12733d2e5037a5d966e336f1f3034b485b86e44
Scott Yourstone <q847694@vsd-cowhand01.eacc.ds.quintiles.com>
2019-07-06 13:35:04 -0400

    added a logger feature

--- python_script.py
+++ python_script.py
@@ -6,6 +6,7 @@
 import os
 import sys
 import argparse
+import logging
 
 
 def main(arguments):
@@ -13,13 +14,18 @@ def main(arguments):
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-i', '--infile', help="Input file", type=argparse.FileType('r'))
+    parser.add_argument('-i', '--infile', help="Input file", 
+            type=argparse.FileType('r'))
     parser.add_argument('-o', '--outfile', help="Output file",
-                        default=sys.stdout, type=argparse.FileType('w'))
+            default=sys.stdout, type=argparse.FileType('w'))
+    parser.add_argument('-l', '--logfile', help="Log file",
+            default="log", type=str)
 
     args = parser.parse_args(arguments)
+
+    logging.basicConfig(filename='log',level=logging.DEBUG)
 
     print(args)
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+aadfasdf    sys.exit(main(sys.argv[1:]))
